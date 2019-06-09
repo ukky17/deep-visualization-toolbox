986b806084aed45bda043eff14fe15aa6abef538
ukky17 <i.love.ny517@gmail.com>
2019-06-09 16:08:09 +0900

    modify for all layers

--- runall.sh
+++ runall.sh
@@ -1,7 +1,22 @@
 #!/usr/bin/env bash
 
 # iterate
+for i in `seq 0 383`
+do
+    ./optimize_image.py --push-layer conv3 --push-channel $i --push-spatial 6
+done
+
+for i in `seq 0 383`
+do
+    ./optimize_image.py --push-layer conv4 --push-channel $i --push-spatial 6
+done
+
 for i in `seq 0 255`
 do
-    ./optimize_image.py --push-layer conv2 --push-channel $i
+    ./optimize_image.py --push-layer conv5 --push-channel $i --push-spatial 6
+done
+
+for i in `seq 0 95`
+do
+    ./optimize_image.py --push-layer conv1 --push-channel $i --push-spatial 27
 done
