a36fb3ab6ffbd870dbd2a90538b05e02a1fe7a7f
ukky17 <i.love.ny517@gmail.com>
2019-06-19 18:05:26 +0900

    add runall.sh for various models

--- runall_alexnet.sh
+++ runall_alexnet.sh
@@ -1,6 +1,19 @@
 #!/usr/bin/env bash
 
+# prepare settings_local.py
+cp models/caffenet-yos/settings_local.template-caffenet-yos.py settings_local.py
+
 # iterate
+for i in `seq 0 95`
+do
+    ./optimize_image.py --push-layer conv1 --push-channel $i --push-spatial 27
+done
+
+for i in `seq 0 255`
+do
+    ./optimize_image.py --push-layer conv2 --push-channel $i --push-spatial 13
+done
+
 for i in `seq 0 383`
 do
     ./optimize_image.py --push-layer conv3 --push-channel $i --push-spatial 6
@@ -14,9 +27,4 @@ done
 for i in `seq 0 255`
 do
     ./optimize_image.py --push-layer conv5 --push-channel $i --push-spatial 6
-done
-
-for i in `seq 0 95`
-do
-    ./optimize_image.py --push-layer conv1 --push-channel $i --push-spatial 27
 done
---
+++ runall_vgg16.sh
@@ -0,0 +1,70 @@
+#!/usr/bin/env bash
+
+# prepare settings_local.py
+cp models/vgg16/settings_local.vgg16.py settings_local.py
+
+# iterate
+for i in `seq 0 63`
+do
+    ./optimize_image.py --push-layer conv1_1 --push-channel $i --push-spatial 112 --data-size 224
+done
+
+for i in `seq 0 63`
+do
+    ./optimize_image.py --push-layer conv1_2 --push-channel $i --push-spatial 112 --data-size 224
+done
+
+for i in `seq 0 127`
+do
+    ./optimize_image.py --push-layer conv2_1 --push-channel $i --push-spatial 56 --data-size 224
+done
+
+for i in `seq 0 127`
+do
+    ./optimize_image.py --push-layer conv2_2 --push-channel $i --push-spatial 56 --data-size 224
+done
+
+for i in `seq 0 255`
+do
+    ./optimize_image.py --push-layer conv3_1 --push-channel $i --push-spatial 27 --data-size 224
+done
+
+for i in `seq 0 255`
+do
+    ./optimize_image.py --push-layer conv3_2 --push-channel $i --push-spatial 27 --data-size 224
+done
+
+for i in `seq 0 255`
+do
+    ./optimize_image.py --push-layer conv3_3 --push-channel $i --push-spatial 27 --data-size 224
+done
+
+for i in `seq 0 511`
+do
+    ./optimize_image.py --push-layer conv4_1 --push-channel $i --push-spatial 14 --data-size 224
+done
+
+for i in `seq 0 511`
+do
+    ./optimize_image.py --push-layer conv4_2 --push-channel $i --push-spatial 14 --data-size 224
+done
+
+for i in `seq 0 511`
+do
+    ./optimize_image.py --push-layer conv4_3 --push-channel $i --push-spatial 14 --data-size 224
+done
+
+for i in `seq 0 511`
+do
+    ./optimize_image.py --push-layer conv5_1 --push-channel $i --push-spatial 7 --data-size 224
+done
+
+for i in `seq 0 511`
+do
+    ./optimize_image.py --push-layer conv5_2 --push-channel $i --push-spatial 7 --data-size 224
+done
+
+for i in `seq 0 511`
+do
+    ./optimize_image.py --push-layer conv5_3 --push-channel $i --push-spatial 7 --data-size 224
+done
